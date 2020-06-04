1d807b6af4dd1b8d39f8ba52eb8526f588ab94e9
Oskar <case112@users.noreply.github.com>
2020-06-04 13:34:10 +0300

    Now can add and delete with jsonplaceholder testapi

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -10,7 +10,8 @@
 <script>
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
-import Header from './components/layout/Header'
+import Header from './components/layout/Header';
+import axios from 'axios';
 
 
 export default {
@@ -22,32 +23,31 @@ export default {
   },
   data() {
     return {
-      todos: [
-        {
-         id: 1,
-         title: 'Todo One',
-         completed: false 
-        },
-        {
-         id: 2,
-         title: 'Todo Two',
-         completed: true
-        },
-        {
-         id: 3,
-         title: 'Todo Three',
-         completed: false 
-        }
-      ]
+      todos: []
     }
   },
   methods: {
     deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id);
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+
+        .then(() => this.todos = this.todos.filter(todo => todo.id !== id))
+        .catch(err => console.log(err));
     },
     addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo];
+      const { title, completed } = newTodo;
+
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+        })
+          .then(res => this.todos = [...this.todos, res.data])
+          .catch(err => console.log(err));
     }
+  },
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then(res => this.todos = res.data)
+      .catch(err => console.log(err));
   }
 }
 </script>
